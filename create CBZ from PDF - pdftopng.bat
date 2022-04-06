8435e54f2dc6235aa1f463ab2d1348e019504b60
kandjar <kandjar@users.noreply.github.com>
2022-04-06 12:17:19 +0200

    Update create CBZ from PDF - pdftopng.bat

--- create CBZ from PDF - pdftopng.bat
+++ create CBZ from PDF - pdftopng.bat
@@ -5,6 +5,7 @@ FOR /R "%~dp0" %%F in (*.pdf) do (
 	pdftopng.exe -aa no -aaVector no -q "%%F" "%~dp0\%%~nF\%%~nF"
 	)
 FOR /D /r %%G in (*) DO (
-	7za.exe a -tzip "%%~nxG.cbz" "%%~nxG\*.*"
+	:: Don't forget to change your drive name if 7zip is not stored at "C:\"
+	"C:\Program Files\7-Zip\7z.exe" a -tzip "%%~nxG.cbz" "%%~nxG\*.*"
 	RMDIR /S /Q "%%~nxG"
 	)
