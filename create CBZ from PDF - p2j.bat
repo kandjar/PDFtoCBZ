8e989b55b0a2ad0f3b09d0fb7eab4d4a3cdc3cb4
kandjar <kandjar@users.noreply.github.com>
2022-04-06 12:17:04 +0200

    Update create CBZ from PDF - p2j.bat

--- create CBZ from PDF - p2j.bat
+++ create CBZ from PDF - p2j.bat
@@ -7,6 +7,7 @@ FOR /R "%~dp0" %%F in (*.pdf) do (
 	)
 	
 FOR /D /r %%G in (*) DO (
-	7za.exe a -tzip "%%~nxG.cbz" "%%~nxG\*.*"
+	:: Don't forget to change your drive name if 7zip is not stored at "C:\"
+	"C:\Program Files\7-Zip\7z.exe" a -tzip "%%~nxG.cbz" "%%~nxG\*.*"
 	RMDIR /S /Q "%%~nxG"
 	)
